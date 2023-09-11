9ea3ba64d12b86131c076cd9ba1c53095e734315
Mandeep Adlab <mandeep@adlabitsolutions.com>
2023-09-12 00:39:51 +0530

    pta nhi

---
+++ test/Config.php
@@ -0,0 +1,23 @@
+sdfsdf<?php
+namespace Adlab\Poitems\Plugin\Catalog\Model;
+class Config
+{
+    /**
+     * Adding custom options and changing labels
+     *
+     * @param \Magento\Catalog\Model\Config $catalogConfig
+     * @param [] $options
+     * @return []
+     */
+    public function afterGetAttributeUsedForSortByArray(\Magento\Catalog\Model\Config $catalogConfig, $options)
+    {
+        // $options['order_by'] = __('order by');
+        $options['name_az'] = __('Product A-Z');
+        $options['name_za'] = __('Product Z-A');
+        // $options['newest'] = __('New arrivals');
+        $options['price_asc'] = __('Price low to high');
+        $options['price_desc'] = __('Price high tolow');
+
+        return $options;
+    }
+}
